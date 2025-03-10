c076653e7eb463f92ee4a7a68c1a3d99342091e9
David O'Donovan <david@davidodonovan.com>
2025-03-10 13:31:10 +1100

    messagesTable schema

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, varchar, serial, timestamp, boolean } from "drizzle-orm/pg-core";
+import { pgTable, varchar, serial, timestamp, boolean, integer } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: serial('id').primaryKey(),
@@ -8,6 +8,13 @@ export const usersTable = pgTable("users", {
   online: boolean('online').default(false).notNull(),
   updated_at: timestamp('updated_at').defaultNow().notNull(),
 });
+
+export const messagesTable = pgTable("messages", {
+  id: serial('id').primaryKey(),
+  created_at: timestamp('created_at').defaultNow().notNull(),
+  sender_id: integer().notNull(),
+  receiver_id: integer().notNull()
+})
 
 export type NewUser = typeof usersTable.$inferInsert;
 export type User = typeof usersTable.$inferSelect;
