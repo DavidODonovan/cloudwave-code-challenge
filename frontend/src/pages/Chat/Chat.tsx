f906a0c2bb8feab522b835b8cad2c631ea84dc5a
David O'Donovan <david@davidodonovan.com>
2025-03-10 21:41:42 +1100

    socket.id changes on Chat page render...

--- frontend/src/pages/Chat/Chat.tsx
+++ frontend/src/pages/Chat/Chat.tsx
@@ -1,31 +1,16 @@
-import { useEffect, useState } from 'react';
 import { Socket } from 'socket.io-client';
 import { useParams } from 'react-router-dom';
 
 
 export default function Chat({ socket }: { socket: Socket }) {
+  if(socket){
+    console.log("incoming socket chat: ", socket);
+  }
   const { userId } = useParams<{ userId: string }>();
-  const [socketId, setSocketId] = useState<string | undefined>();
-
-  useEffect(()=>{
-      if(socket.connected) {
-        setSocketId(socket.id)
-      };
-
-      socket?.on('connect', ()=>{
-        setSocketId(socket.id)
-      });
-
-      socket?.on('disconnect', ()=>{
-        setSocketId(undefined)
-      });
-
-  }, [socket]);
 
   return <div>
     <h1>Chat</h1>
     <p>userId: {userId}</p>
-    {socket.connected ? <p>Connected</p> : <p>Not connected</p>}
-    <div>socketId: {socketId}</div>
+    <div>{socket ? <p>{socket.id}</p> : <p>Not connected</p>}</div>
   </div>;
 }
