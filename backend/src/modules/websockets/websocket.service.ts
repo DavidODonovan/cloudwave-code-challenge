e0bc1a087dc212890379e67785c8ad1afec44854
David O'Donovan <david@davidodonovan.com>
2025-03-10 13:39:45 +1100

    basic websocket service methods

--- backend/src/modules/websockets/websocket.service.ts
+++ backend/src/modules/websockets/websocket.service.ts
@@ -7,5 +7,26 @@ class WebSocketService {
       private io: IOServer,
       private db: NodePgDatabase
     ) {
+        this.initialize();
     }
-}
+
+    initialize(): void {
+        this.io.on('connection', this.handleConnection.bind(this));
+    }
+
+    handleConnection(socket: Socket): void {
+        socket.on('message', this.handleMessage.bind(this));
+        socket.on('register', this.handleRegister.bind(this));
+    }
+
+    handleMessage(message: any): void {
+        console.log('message received', message);
+        this.io.emit('message', message);
+        console.log('message sent to all clients');
+        // emit message event to recipient and save message to database messages table.
+    }
+
+    handleRegister({ user_id, socket_id }: { user_id: string, socket_id: string }): void {
+        console.log('new register event:', user_id, socket_id);
+    }
+};
