ff86511d657a732f7aa79c9afc039ecdcbb2ef53
David O'Donovan <david@davidodonovan.com>
2025-03-11 10:19:50 +1100

    update start script, userlist

--- backend/src/utils/seedFakeUsers.ts
+++ backend/src/utils/seedFakeUsers.ts
@@ -2,7 +2,7 @@ import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { usersTable } from '../db/schema';
 
 export const seedFakeUsers = async (db: NodePgDatabase) => {
-    const userNames = ['Lena13', 'Libby11', 'Lucky1', 'Janina1', 'Melissa32', 'PeteTheGreat', 'SallySellsSeaShells', 'SuziQ', 'Bretto', 'Lester'];
+    const userNames = ['Lena13', 'Libby11', 'Lucky1', 'Janina1', 'Melissa32', 'PeteTheGreat', 'SallyCinnamon', 'SuziQ', 'Bretto', 'Lester'];
     try {
       await Promise.all(userNames.map(async (name) => {
         await db.insert(usersTable).values({ name });
--- start-app.sh
+++ start-app.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# Default number of fake data items
-NUM_ITEMS=10
-
-# Parse command-line arguments
-while getopts ":n:" opt; do
-  case $opt in
-    n) NUM_ITEMS=$OPTARG ;;
-    \?) echo "Invalid option -$OPTARG" >&2; exit 1 ;;
-  esac
-done
-
 # Start the backend Docker containers
 echo "Starting Docker containers..."
 docker compose up -d
@@ -20,11 +9,6 @@ if [ $? -ne 0 ]; then
     echo "Failed to start Docker containers. Exiting."
     exit 1
 fi
-
-# # Run the seed script with the specified number of fake data items
-# echo "Seeding database with $NUM_ITEMS fake data items..."
-# sleep 15  # Wait for 15 seconds to ensure the API is ready
-# docker run --rm --network="host" -v $(pwd)/backend:/backend node:20-alpine sh -c "node /backend/seed.js $NUM_ITEMS"
 
 # Navigate to the frontend directory
 cd frontend
